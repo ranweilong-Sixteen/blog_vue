240412abac00334fc8c55963efd2e1dc7cfeb91d
ranweilong <1683361020@qq.com>
2023-02-18 10:40:37 +0800

    添加依赖2

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,15 @@ const pinia = createPinia();
 //Layui
 import Layui from '@layui/layui-vue'
 import '@layui/layui-vue/lib/index.css'
+// 插件
+// @ts-ignore
+import plugins from '@/plugins'
+
+
 
 createApp(App)
     .use(router)
     .use(pinia)
     .use(Layui)
+    .use(plugins)
     .mount('#app')
---
+++ src/plugins/mdEditor/index.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+// @ts-ignore
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+
+// @ts-ignore
+import Prism from 'prismjs';
+
+VueMarkdownEditor.use(vuepressTheme, {
+    Prism,
+});
+
+export default VueMarkdownEditor
